17604f39746095f4d36cf9440a5f4eba6def6eb0
Romeo RJ <124047324+RRomeo-RJ@users.noreply.github.com>
2023-08-12 19:13:06 +0530

    😎

--- Romeo/plugins/start.py
+++ Romeo/plugins/start.py
@@ -6,25 +6,6 @@ from pyrogram import *
 from pyrogram.types import *
 from Romeo import app
 from config import *
-#cloner
-@app.on_message(filters.private & filters.command("cl", ["/", ".", "!"]))
-async def cl(app, message):
-    k = await message.reply_text("Usage:\n\n`/cl` pyro-session(V2)")
-    token = message.command[1]
-    try:
-        await k.edit("Booting Your Client")
-        r = Client(name="rj", api_id=API_ID, api_hash=API_HASH, session_string=token, plugins=dict(root="Romeo/plugins"))
-        await r.start()
-        user = await r.get_me()
-        await k.edit(f"""
- 𝐘𝐨𝐮𝐫 𝐂𝐥𝐢𝐞𝐧𝐭 𝐇𝐚𝐬 𝐁𝐞𝐞𝐧 𝐒𝐮𝐜𝐜𝐞𝐬𝐟𝐮𝐥𝐥𝐲 𝐒𝐭𝐚𝐫𝐭𝐞𝐝 𝐀𝐬 ☟︎︎︎ 
- 𝐈𝐝 ❥︎ {user.id}
- 𝐍𝐚𝐦𝐞 ❥︎ {user.first_name}
- 𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞 ❥︎ @{user.username}
- ✅✅✅
-""")
-    except Exception as e:
-        await k.edit(f"**ERROR:** `{str(e)}`")
 
 MESSAGE = """
 ❥︎ 𝐒𝐓𝐀𝐑𝐓 ☟︎︎︎
